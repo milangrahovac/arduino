e334ddc25c9232055656964715251f0c58dc62fe
Milan Grahovac <grahovac85@gmail.com>
2018-12-28 16:58:47 +0100

    add code

--- lcd_display_clock_and_temp/lcd_display_clock_and_temp.ino
+++ lcd_display_clock_and_temp/lcd_display_clock_and_temp.ino
@@ -20,17 +20,26 @@ void setup () {
   while (!Serial); // for Leonardo/Micro/Zero
 #endif
 
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  delay(3000); // wait for console opening
+    delay(3000); // wait for console opening
 
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
+    if (! rtc.begin()) {
+        Serial.println("Couldn't find RTC");
+        lcd.setCursor(0,0);
+        lcd.print(String("Couldn't find RTC");
     while (1);
   }
 
   if (rtc.lostPower()) {
     Serial.println("RTC lost power, lets set the time!");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(String("RTC lost power,");
+    lcd.setCursor(0,1);
+    lcd.print(String("lets set the time!");
+    delay(2000);
+
     // following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
@@ -41,10 +50,10 @@ void setup () {
 
 
 void TimeAndDate () {
-
+    
     lcd.clear();
-
-   for (int i=0; i <= 5; i++){
+    
+    for (int i=0; i <= 5; i++){
 
         DateTime now = rtc.now();
         
@@ -56,7 +65,6 @@ void TimeAndDate () {
         
         lcd.setCursor(4,0);
         lcd.print(bufTime);
-
         lcd.setCursor(0,1);
         lcd.print(daysOfTheWeek[now.dayOfTheWeek()] + String("   ") +  bufDate);
         
@@ -68,18 +76,18 @@ void TimeAndDate () {
 
 
 void temperature () {
-  DHT.read11(DHTPIN);
-  lcd.clear();
-  lcd.setCursor(2,0);
-  lcd.print(String("Temp:  ") + round(DHT.temperature) + String(" ") + (char) 223 + String("C"));
-  lcd.setCursor(1,1);
-  lcd.print(String("Humidity: ") + round(DHT.humidity) + String(" %"));
-  delay(5000);
+    DHT.read11(DHTPIN);
+    lcd.clear();
+    lcd.setCursor(2,0);
+    lcd.print(String("Temp:  ") + round(DHT.temperature) + String(" ") + (char) 223 + String("C"));
+    lcd.setCursor(1,1);
+    lcd.print(String("Humidity: ") + round(DHT.humidity) + String(" %"));
+    delay(5000);
 }
 
 
 
 void loop () {
-  TimeAndDate();
-  temperature();
+    TimeAndDate();
+    temperature();
 }
